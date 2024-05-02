8ffa0ee8b99fb5eedd1884bd7ba4a1d3a24a6c38
stas <stas.sklyar.dev@gmail.com>
2024-05-02 11:10:29 -0700

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Autocomplete
+      multiple
+      options={options}
+      getOptionLabel={(option) => option.title}
+      defaultValue={[]}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="standard"
+          placeholder='Enter value'
+        />
+      )}
+      style={{ width: '70%', margin: '50px auto' }}
+    />
   );
-}
+};
+
+const options = [
+  { title: '+', value: '+' },
+  { title: '-', value: '-' },
+  { title: '/', value: '/' },
+  { title: '*', value: '*' },
+  { title: '(', value: '(' },
+  { title: ')', value: ')' },
+
+  { title: '1 test', value: 1 },
+  { title: '2 test', value: 42 },
+  { title: 'another test', value: 13 },
+  { title: 'one more test', value: 2000 },
+];
 
 export default App;
